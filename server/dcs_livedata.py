c9054b7141b8ce27e312e5dd68928850ea8f2941
Brian Maranville <brian.maranville@nist.gov>
2014-06-30 15:44:06 -0400

    adding dcs plotting and updating singleplotwindow log/lin transform

---
+++ server/dcs_livedata.py
@@ -0,0 +1,117 @@
+import glob
+import os
+import sys
+sys.path.append('/var/www/')
+sys.path.append('/home/bbm/')
+import paramiko
+import urllib2, ftplib
+import time
+import StringIO
+
+RETRIEVE_METHOD = "ssh" # or "ftp" or "urllib"
+MAX_FTP_RETRIES = 5
+
+source_host = "solo.ncnr.nist.gov"                  #hard-coded
+source_path = "/var/ftp"
+source_port = 22
+
+sources = [
+    {"name": "DCS", 
+     "root_dir": "/home/ncnr/",
+     "live_datapath":"livedata",
+     "live_dataname": "livedata.dcs.gz"},
+]
+
+output = {}
+output_filelike = {}
+
+local_path = "~/.livedata/DCS/'
+
+dest_host = "webster.ncnr.nist.gov"                    #hard-coded
+dest_port = 22
+
+# I have a different key for pushing to webster.
+dest_pkey = paramiko.RSAKey(filename='/home/bbm/.ssh/datapushkey')
+dest_username = "bbm"
+
+
+# writing a list of the last file in Javascript format to a file
+json_filename = 'live_data.json'
+
+if RETRIEVE_METHOD == "ssh":
+    source_transport = paramiko.Transport((source_host, source_port))
+    source_pkey = paramiko.RSAKey(filename="/home/bbm/.ssh/datapullkey")
+    source_username = "ncnr"
+    source_transport.connect(username=source_username, pkey = source_pkey)
+    source_sftp = paramiko.SFTPClient.from_transport(source_transport)
+    
+for source in sources:
+    live_dataname = source['live_dataname']
+    live_datapath = source['live_datapath']
+    root_dir = source['root_dir']
+    name = source['name']
+    #print "live data modified:", source_sftp.file(live_dataname).stat().st_mtime
+    
+    #live_data = StringIO.StringIO()
+    live_data = open(os.path.join(local_path, live_dataname), 'wb')
+    if RETRIEVE_METHOD == "urllib":       
+        req_addr = os.path.join("ftp://" + source_host, live_datapath, live_dataname)
+        #req = urllib2.Request(req_addr)
+        response = None
+        retries = 0
+        while retries < MAX_FTP_RETRIES:
+            try:
+                response = urllib2.urlopen(req_addr)
+                break
+            except:
+                print "failed attempt %d to retrieve %s: trying again" % (retries, req_addr)
+            retries += 1
+        
+                
+        if response is None: continue
+        print "retrieved %s" % (req_addr)
+        live_data.write(response.read())
+    
+    elif RETRIEVE_METHOD == "ftp":
+        ftp = ftplib.FTP(source_host)
+        ftp.login('anonymous')
+        ftp.cwd(live_datapath)
+        ftp.retrbinary("RETR " + live_dataname, live_data.write)
+        ftp.close()
+        
+    elif RETRIEVE_METHOD == "ssh":
+        f = source_sftp.open(os.path.join(root_dir, live_datapath, live_dataname))
+        response = f.read()
+        f.close()
+        live_data.write(response)
+        
+    else:
+        print "no valid RETRIEVE_METHOD"
+      
+    #live_data.seek(0) # move back to the beginning of file
+    live_data.close()
+    
+    files = [live_dataname,]
+
+    # here I import the library that reads in SANS files:
+    from plot_dcs import process_raw_dcs
+    json_data = process_raw_dcs(local_path)
+    
+    output[name] = json_data
+    
+    dest_transport = paramiko.Transport((dest_host, dest_port))
+    dest_transport.connect(username = dest_username, pkey = dest_pkey)
+    dest_sftp = paramiko.SFTPClient.from_transport(dest_transport)
+   
+    name = source['name']
+
+    # now I push that file outside the firewall to webster:
+    remotepath = os.path.join('ipeek', 'data', name, json_filename)
+
+    f = dest_sftp.open(remotepath, 'w')
+    f.write(output[name])
+    f.close()
+
+    dest_sftp.close()
+    dest_transport.close()
+#print 'Upload done.'
---
+++ server/plot_dcs.py
@@ -0,0 +1,107 @@
+import h5py
+import simplejson
+import os
+import numpy as np
+#import matplotlib.pyplot as plt
+from time import time
+
+def Elam(lam):
+    """
+    convert wavelength in angstroms to energy in meV
+    """
+    return 81.81/lam**2
+
+def Ek(k):
+    """
+    convert wave-vector in inver angstroms to energy in meV
+    """
+    return 2.072*k**2
+
+def kE(E):
+    return np.sqrt(E/2.072)
+
+def Qfunc(ki, kf, theta):
+    """
+    evaluate the magnitude of Q from ki, kf, and theta
+    theta is the angle between kf and ki, sometimes called 2 theta, units of degrees
+    """
+    return np.sqrt(  ki**2 + kf**2 - 2*ki*kf*np.cos(theta*np.pi/180)  )
+
+def Ef_from_timechannel(timeChannel, t_SD_min, speedRatDenom, masterSpeed):
+    """
+    using the parameters
+        t_SD_min = minimum sample to detector time
+        speedRatDenom = to set FOL chopper speed
+        masterSpeed = chopper speed (except for FOL chopper)
+    using the variabl
+        timeChannel, where I am numbering from 1 <be careful of this convention>
+    """
+    return 8.41e7 / (t_SD_min + (timeChannel+1)*    (6e4 *(speedRatDenom/masterSpeed))   )**2
+
+def process_raw_dcs(data_path):
+    # t0 = time()
+    os.chdir(data_path) # change working directory
+    detInfo = np.genfromtxt('dcs_detector_info.txt', skip_header=1, skip_footer=17)
+    detToTwoTheta = detInfo[:,9] # 10th column
+
+
+    os.system('gzip -dc livedata.dcs.gz > livedata.dcs')
+    #os.system('C:\\Software\\Octave-3.6.4\\bin\\octave --eval "load livedata.dcs; save -hdf5 livedata.hdf;"')
+    os.system('octave --eval "load livedata.dcs; save -hdf5 livedata.hdf;"') 
+    f = h5py.File('livedata.hdf')
+    data = f['histodata']['value'].value
+
+    ch_wl = f['ch_wl']['value'].value
+    Ei = Elam(ch_wl)
+    ki = kE(Ei)
+    dE =0.5*(-0.10395+0.05616 *Ei+0.00108 *Ei**2) #take the putative resolution and halve it
+    masterSpeed = f['ch_ms']['value'].value
+    speedRatDenom = f['ch_srdenom']['value'].value
+    t_SD_min = f['tsdmin']['value'].value
+
+    Q_max = Qfunc(ki,ki,150)
+    Q_min = 0
+    E_bins = np.linspace(-Ei, Ei, int(2*Ei/dE) )
+    Q_bins = np.linspace(Q_min,Q_max,301)
+
+    #for every point in {timechannel, detectorchannel} space, map into a bin of {E,Q} space
+    #remember, data is organized as data[detectorchannel][timechannel]
+    i,j = np.indices(data.shape)
+    ef = Ef_from_timechannel(j, t_SD_min, speedRatDenom, masterSpeed)
+    Q_ = Qfunc(ki, kE(ef), detToTwoTheta[:, None])
+
+    E_transfer = Ei-ef
+    E_mask = (E_transfer > -Ei)
+
+    EQ_data, xedges, yedges = np.histogram2d(Q_[E_mask], E_transfer[E_mask], bins=(Q_bins, E_bins), range=([Q_min,Q_max], [-Ei, Ei]), weights=data[E_mask])
+
+
+    stop_date = ''.join(chr(a) for a in f['stop_date']['value'].value.flatten())
+    start_date = ''.join(chr(a) for a in f['start_date']['value'].value.flatten())
+            
+    output = {
+        "title": "DCS snapshot",
+        "dims": {
+            "ymin": 0-Ei,
+            "ymax": EQ_data.shape[0]*0+Ei,
+            "ydim": EQ_data.shape[0],
+            "xmin": 0,
+            "xmax": EQ_data.shape[1]*0+np.max(Q_bins),
+            "xdim": EQ_data.shape[1],
+            "zmin": EQ_data.min(),
+            "zmax": EQ_data.max()
+        },
+        "type": "2d",
+        "xlabel": "Ei-Ef [meV]",
+        "ylabel": "|Q| [/AA^-1]",
+        "z": [EQ_data.tolist()],
+        "metadata": {
+            "stop_date": stop_date,
+            "start_date": start_date
+        }
+    }
+
+    #print time()-t0
+    return simplejson.dumps([output])
+
+
