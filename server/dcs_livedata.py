9ce64bbe83ae81ba842d646008282c89dd0080f6
Brian Benjamin Maranville <brian.maranville@nist.gov>
2014-09-02 15:21:53 -0400

    updates to the live data puller

--- server/dcs_livedata.py
+++ server/dcs_livedata.py
@@ -11,14 +11,25 @@ import StringIO
 RETRIEVE_METHOD = "ssh" # or "ftp" or "urllib"
 MAX_FTP_RETRIES = 5
 
-source_host = "solo.ncnr.nist.gov"                  #hard-coded
-source_path = "/var/ftp"
-source_port = 22
+#source_host = "solo.ncnr.nist.gov"                  #hard-coded
+#source_path = "/var/ftp"
+#source_port = 22
 
 sources = [
-    {"name": "DCS", 
+    {"name": "DCS",
+     "host_name": "solo.ncnr.nist.gov",
+     "host_port": 22,
+     "retrieve_method": "ssh",
      "root_dir": "/home/NIST/ncnr/",
      "live_datapath":"livedata",
+     #"live_dataname": "livedata.dcs.gz"},
+     "live_dataname": "live_data.json"},
+     {"name": "NSE",
+     "host_name": "echo.ncnr.nist.gov",
+     "host_port": 22,
+     "retrieve_method": "ssh",
+     "root_dir": "/usr/local/nice/server_data/experiments/",
+     "live_datapath":"live_data",
      #"live_dataname": "livedata.dcs.gz"},
      "live_dataname": "live_data.json"},
 ]
@@ -26,7 +37,7 @@ sources = [
 output = {}
 output_filelike = {}
 
-local_path = "/home/bbm/.livedata/DCS/"
+#local_path = "/home/bbm/.livedata/DCS/"
 
 dest_host = "webster.ncnr.nist.gov"                    #hard-coded
 dest_port = 22
@@ -39,23 +50,21 @@ dest_username = "bbm"
 # writing a list of the last file in Javascript format to a file
 json_filename = 'live_data.json'
 
-if RETRIEVE_METHOD == "ssh":
-    source_transport = paramiko.Transport((source_host, source_port))
-    source_pkey = paramiko.RSAKey(filename="/home/bbm/.ssh/datapullkey")
-    source_username = "ncnr"
-    source_transport.connect(username=source_username, pkey = source_pkey)
-    source_sftp = paramiko.SFTPClient.from_transport(source_transport)
+
     
 for source in sources:
+    retrieve_method = source['retrieve_method']
     live_dataname = source['live_dataname']
     live_datapath = source['live_datapath']
     root_dir = source['root_dir']
     name = source['name']
+    source_host = source['host_name']
+    source_port = source['host_port']
     #print "live data modified:", source_sftp.file(live_dataname).stat().st_mtime
     
     live_data = StringIO.StringIO()
     #live_data = open(os.path.join(local_path, live_dataname), 'wb')
-    if RETRIEVE_METHOD == "urllib":       
+    if retrieve_method == "urllib":       
         req_addr = os.path.join("ftp://" + source_host, live_datapath, live_dataname)
         #req = urllib2.Request(req_addr)
         response = None
@@ -73,21 +82,27 @@ for source in sources:
         print "retrieved %s" % (req_addr)
         live_data.write(response.read())
     
-    elif RETRIEVE_METHOD == "ftp":
+    elif retrieve_method == "ftp":
         ftp = ftplib.FTP(source_host)
         ftp.login('anonymous')
         ftp.cwd(live_datapath)
         ftp.retrbinary("RETR " + live_dataname, live_data.write)
         ftp.close()
         
-    elif RETRIEVE_METHOD == "ssh":
+    elif retrieve_method == "ssh":
+        source_transport = paramiko.Transport((source_host, source_port))
+        source_pkey = paramiko.RSAKey(filename="/home/bbm/.ssh/datapullkey")
+        source_username = "ncnr"
+        source_transport.connect(username=source_username, pkey = source_pkey)
+        source_sftp = paramiko.SFTPClient.from_transport(source_transport)
+        
         f = source_sftp.open(os.path.join(root_dir, live_datapath, live_dataname))
         response = f.read()
         f.close()
         live_data.write(response)
         
     else:
-        print "no valid RETRIEVE_METHOD"
+        print "no valid retrieve_method"
       
     live_data.seek(0) # move back to the beginning of file
     #live_data.close()
@@ -100,11 +115,13 @@ for source in sources:
     json_data = live_data    
 
     output[name] = json_data.read()
-    
-    dest_transport = paramiko.Transport((dest_host, dest_port))
-    dest_transport.connect(username = dest_username, pkey = dest_pkey)
-    dest_sftp = paramiko.SFTPClient.from_transport(dest_transport)
-   
+
+# Now initialize the transfer to the destination:    
+dest_transport = paramiko.Transport((dest_host, dest_port))
+dest_transport.connect(username = dest_username, pkey = dest_pkey)
+dest_sftp = paramiko.SFTPClient.from_transport(dest_transport)
+
+for source in sources:   
     name = source['name']
 
     # now I push that file outside the firewall to webster:
@@ -114,6 +131,6 @@ for source in sources:
     f.write(output[name])
     f.close()
 
-    dest_sftp.close()
-    dest_transport.close()
+dest_sftp.close()
+dest_transport.close()
 #print 'Upload done.'
