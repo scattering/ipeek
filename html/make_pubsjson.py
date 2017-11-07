7edd5e61fe23ccd4e40e80d40c1e441516434b54
bbm <brian.maranville@nist.gov>
2017-11-07 15:33:19 -0500

    filtering keys for output, to make it smaller... and using try block to skip DOI elements that don't load

--- html/make_pubsjson.py
+++ html/make_pubsjson.py
@@ -1,17 +1,42 @@
 import requests
 import json
+import urllib
 
 json_headers = {"Accept": "application/vnd.citationstyles.csl+json"}
 bibtex_headers = {"Accept": "application/x-bibtex"}
 
-def make_pubdb(doi_list):
+keys_to_import = [
+    "title",
+    "author",
+    "container-title-short",
+    "container-title",
+    "volume",
+    "page",
+    "article-number",
+    "issued",
+    "DOI",
+    "URL",
+    "is-referenced-by-count"
+]
+
+def make_pubdb(doi_list, add_bibtex=False):
     output = []
-    for doi in doi_list:
-        rj = requests.get("https://doi.org/%s" % (doi,), headers = json_headers)
-        entry = rj.json()
-        rb = requests.get("https://doi.org/%s" % (doi,), headers = bibtex_headers)
-        entry["__bibtex"] = rb.text
-        output.append(entry)
+    for i, doi in enumerate(doi_list):
+        escaped_doi = urllib.parse.quote(doi)
+        print(i, escaped_doi)
+        rj = requests.get("https://data.crossref.org/%s" % (escaped_doi,), headers = json_headers)
+        #rj = requests.get("https://doi.org/%s" % (doi,), headers = json_headers)
+        entry = {}
+        try: 
+            content = rj.json()
+            entry.update(dict([(k, content.get(k, None)) for k in keys_to_import]))
+            if add_bibtex:
+                rb = requests.get("https://doi.org/%s" % (doi,), headers = bibtex_headers)
+                entry["__bibtex"] = rb.text
+            output.append(entry)
+        except Exception:
+            print("not processing %s because of error: text" % (doi,))
+        
     return output
     
 if __name__ == '__main__':
@@ -19,7 +44,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         input_filename = sys.argv[1]
         doi_list = json.loads(open(input_filename, "r").read())
-        result = json.dumps(make_pubdb(doi_list), indent=2)
+        result = json.dumps(make_pubdb(doi_list))
         if len(sys.argv) > 2:
             open(sys.argv[2], "w").write(result)
         else:
