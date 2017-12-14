0d3e368f74b31cb14649245100ebbc047d0b974e
bbm <brian.maranville@nist.gov>
2017-12-14 16:28:54 -0500

    add http path lister for xraydata

---
+++ html/listhttpfiles.php
@@ -0,0 +1,33 @@
+<?php header('Content-Type: application/json'); ?>
+<?php 
+function list_files($dir) 
+{ 
+    $root = scandir($dir);
+    $subdir_list = array();
+    $files_list = array();
+    $files_metadata = array(); 
+    foreach($root as $value) 
+    { 
+        if($value === '.' || $value === '..') {continue;} 
+        if(is_file("$dir/$value")) {
+            $files_list[] = $value;
+            $files_metadata[$value] = array('mtime' => filemtime("$dir/$value"));
+            continue;}
+        if(is_dir("$dir/$value")) {$subdir_list[]=$value;continue;}
+    } 
+    return array("subdirs" => $subdir_list, "files" => $files_list, "pathlist" => explode("/", $dir), "files_metadata" => $files_metadata); 
+} 
+?>
+
+<?php
+    //echo join('", "', glob("*.html"));
+    $allowed_path = "/var/www/html";
+    $path = $allowed_path;
+    if (!empty($_POST) && !empty($_POST["pathlist"])) {
+        $path .= "/";
+        $path .= join("/", $_POST["pathlist"]);
+        $path = realpath($path);
+    }
+    if (!(strpos($path, $allowed_path) === 0)) { echo '{"error": "illegal request", "path": "' . $path . '"}'; }
+    else { echo json_encode(list_files($path)); }
+?>
