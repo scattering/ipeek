ed20d6abac646511f73d7275614c5f08a337e59e
bbm <brian.maranville@nist.gov>
2018-01-25 12:19:11 -0500

    adding experimental zipfile detection

--- html/listftpfiles.php
+++ html/listftpfiles.php
@@ -12,6 +12,12 @@ function list_files($dir)
         if(is_file("$dir/$value")) {
             $files_list[] = $value;
             $files_metadata[$value] = array('mtime' => filemtime("$dir/$value"));
+            $zip = zip_open("$dir/$value");
+            $is_zipfile = is_resource($zip);
+            array('is_zipfile' => $is_zipfile);
+            if ($is_zipfile) {
+                zip_close($zip);
+            }
             continue;}
         if(is_dir("$dir/$value")) {$subdir_list[]=$value;continue;}
     } 
